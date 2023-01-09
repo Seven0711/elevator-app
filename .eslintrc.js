9231cbf268f5f630d405e29962404e5c3d59f24e
zhangzhitang <zt17534@zongteng.net>
2023-01-09 17:27:24 +0800

    feat:项目初始化

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,9 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/multi-word-component-names": 0,
+    "@typescript-eslint/no-explicit-any": ["off"],
+    "@typescript-eslint/ban-ts-comment": ["off"]
   },
   overrides: [
     {
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    {{ account }}
+    <button @click="handleClick">点击</button>
   </div>
 </template>
+<script lang="ts">
+import { ref, onMounted } from 'vue'
+</script>
+
+<script lang="ts" setup>
+const account = ref<number>(0)
+const handleClick = () => {
+  account.value++
+}
+onMounted(() => {
+  account.value = 10
+})
+</script>
