fe158a53409a226b26dfa00ed2c4fd1ce902733d
zhangzhitang <zt17534@zongteng.net>
2023-01-10 17:59:00 +0800

    feat:eslint配置完善

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,13 @@ module.exports = {
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "vue/multi-word-component-names": 0,
     "@typescript-eslint/no-explicit-any": ["off"],
-    "@typescript-eslint/ban-ts-comment": ["off"]
+    "@typescript-eslint/ban-ts-comment": ["off"],
+    "prettier/prettier": [ // 不去校验换行类型是LF或CRLF
+      "error",
+      {
+        "endOfLine": "auto"
+      },
+    ],
   },
   overrides: [
     {
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,23 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+const resolve = (dir) => path.join(__dirname, dir)
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     port: 9527,
+    host: 'localhost',
     hot: true,
     open: true
+  },
+  chainWebpack: (config) => {
+    config.resolve.alias.set('@', resolve('src'))
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap((options) => {
+        options.compilerOptions = { preserveWhitespace: true }
+        return options
+      })
   }
-});
+})
